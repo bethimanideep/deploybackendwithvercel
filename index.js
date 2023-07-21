5e8dcbd06f1fe736959938c6cdf7b75eea2b191e
bethimanideep <bethimanideep@gmail.com>
2023-07-22 03:51:05 +0530

    last

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const { connection } = require("./config/db");
 require("dotenv").config()
 const { userrouter } = require("./routes/user_route");
 const app = express()
-app.use(express.json())
-const cors=require('cors');
+app.use(express.json()) 
+const cors = require('cors');
 const { clientRoute } = require("./routes/client");
 const { lawyerRoute } = require("./routes/lawyers");
 app.use(cors())
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('welcome')
 })
-app.use("/user",userrouter)
-app.use("/client",clientRoute)
-app.use("/lawyer",lawyerRoute)
+app.use("/user", userrouter)
+app.use("/client", clientRoute)
+app.use("/lawyer", lawyerRoute)
 
-app.listen(process.env.PORT,async ()=>{
+app.listen(process.env.PORT, async () => {
     try {
         await connection
         console.log("connected to db");
